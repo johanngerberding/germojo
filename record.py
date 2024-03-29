37d5ebc1be7210f1e720ecc86a73dfd061fb5cae
johanngerberding <johann.gerberding@lector.ai>
2024-03-29 16:26:01 +0100

    this is fun

--- record.py
+++ record.py
@@ -1,27 +1,45 @@
+import sys
+import wave
+
 import pyaudio
-import wave 
-import sys 
+import whisper
+from TTS.api import TTS
 
+device = "cpu"
+# Load models
 
-FORMAT = pyaudio.paInt8 
+model = whisper.load_model("base")
+tts = TTS("tts_models/de/thorsten/tacotron2-DDC").to(device)
+
+FORMAT = pyaudio.paInt16 
 CHANNELS = 1 if sys.platform == 'darwin' else 2 
-RATE = 44100 
 CHUNK = 1024 
 RECORD_SECONDS = 5 
-WAVE_OUTPUT_FILENAME = "test.wav"
 
 with wave.open("output.wav", "wb") as wf: 
     p = pyaudio.PyAudio()
+    sample_rate = p.get_default_input_device_info()['defaultSampleRate']
+    
     wf.setnchannels(CHANNELS)
     wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
+    wf.setframerate(int(sample_rate))
 
-    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True)
+    stream = p.open(format=FORMAT, channels=CHANNELS, rate=int(sample_rate), input=True)
 
     print("Recording...")
-    for _ in range(0, RATE // CHUNK * RECORD_SECONDS): 
+    for _ in range(0, int(sample_rate) // CHUNK * RECORD_SECONDS): 
         wf.writeframes(stream.read(CHUNK))
     print("Done.")
 
     stream.close()
     p.terminate()
+
+
+result = model.transcribe("output.wav")
+print(result["text"])
+
+tts.tts_with_vc_to_file(
+    result["text"], 
+    speaker_wav="voice.wav",
+    file_path="generated_output.wav"
+)
