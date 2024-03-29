5cea23410294ce3d9bd581423f59a26a4d6ed80c
johanngerberding <johann.gerberding@lector.ai>
2024-03-29 17:37:47 +0100

    this is real fun

--- README.md
+++ README.md
@@ -4,9 +4,13 @@ I want to build my own alexa kinda thing called Mojo, a chatbot that you can tal
 
 ## Ideas 
 
-* use a small mistral model for language and Q&A 
-* distil-whisper for speech to text ?
-* text to speech ?? I have to do some research here
+* use pyaudio to record what you say 
+* use whisper for speech to text 
+* LLM for answering 
+* text to speech 
+* pyaudio for output
+
+* how to really let it run continuously to have a conversation? 
 
 ## Personality 
 
@@ -15,3 +19,5 @@ Mojo should be a funny and smart assistant that you can ask stuff that you are i
 ## References 
 
 * [vllm](https://docs.vllm.ai/en/latest/index.html)
+* [TTS](https://github.com/coqui-ai/TTS)
+* [whisper](https://github.com/openai/whisper)
--- chat.py
+++ chat.py
@@ -1,20 +1,75 @@
+import sys
+import wave
 from string import Template
+
+import pyaudio
+import whisper
+from TTS.api import TTS
 from vllm import LLM, SamplingParams
 
+# pyaudio config
+FORMAT = pyaudio.paInt16 
+CHANNELS = 1 if sys.platform == 'darwin' else 2 
+CHUNK = 1024 
+RECORD_SECONDS = 5 
+
+# LLM params
 sampling_params = SamplingParams(temperature=0.25, top_p=0.95, max_tokens=256)
-
-model = "TheBloke/Llama-2-13B-German-Assistant-v4-AWQ"
-llm = LLM(model=model, quantization="awq")
-
 template = Template("""$personality\nUser: $prompt\nAsssistant:""")
 personality = "Du bist ein hilfreicher Assistent mit dem Namen Mojo. Du bist hilfreich und humorvoll."
-question = "User: Wer bist du und wie geht es dir? Assistant:"
+device = "cpu"
+model = "TheBloke/Llama-2-13B-German-Assistant-v4-AWQ"
+
+# Init models
+llm = LLM(model=model, quantization="awq")
+speech_to_text = whisper.load_model("base")
+text_to_speech = TTS("tts_models/de/thorsten/tacotron2-DDC").to(device)
+
+p = pyaudio.PyAudio()
+sample_rate = p.get_default_input_device_info()['defaultSampleRate']
+
+with wave.open("output.wav", "wb") as wf: 
+    
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(int(sample_rate))
+
+    stream = p.open(format=FORMAT, channels=CHANNELS, rate=int(sample_rate), input=True)
+
+    print("Recording...")
+    for _ in range(0, int(sample_rate) // CHUNK * RECORD_SECONDS): 
+        wf.writeframes(stream.read(CHUNK))
+    print("Done.")
+
+    stream.close()
+
+
+result = speech_to_text.transcribe("output.wav")
+transcription = result["text"]
 
 outputs = llm.generate(
-    prompts=template.substitute(personality=personality, prompt=question),
+    prompts=template.substitute(personality=personality, prompt=transcription),
     sampling_params=sampling_params,
 )
 
-print(template.substitute(personality=personality, prompt=question))
-print(outputs[0].outputs[0].text)
+text_to_speech.tts_with_vc_to_file(
+    outputs[0].outputs[0].text, 
+    speaker_wav="voice.wav",
+    file_path="generated_output.wav"
+)
 
+with wave.open("generated_output.wav", "rb") as wf: 
+    stream = p.open(
+        format=p.get_format_from_width(wf.getsampwidth()),
+        channels=wf.getnchannels(),
+        rate=wf.getframerate(),
+        output=True,
+    )
+    data = wf.readframes(CHUNK)
+    while data: 
+        stream.write(data)
+        data = wf.readframes(CHUNK)
+
+    stream.close()
+
+p.terminate()
